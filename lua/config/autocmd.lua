a06324e36dfc6ceddb70d5cbf0f825abb23ba773
Bread <188591972+BreadOnPenguins@users.noreply.github.com>
2025-07-09 15:35:53 -0400

    Update autocmd.lua

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -75,3 +75,23 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end
 end,
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local startuptime = vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time))
+    vim.g.startup_time_ms = string.format("%.2f ms", startuptime * 1000)
+  end,
+})
+
+vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BDeletePre *",
+	group = "alpha_on_empty",
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local name = vim.api.nvim_buf_get_name(bufnr)
+		if name == "" then
+      vim.cmd([[:Alpha | bd#]])
+		end
+	end,
+})
